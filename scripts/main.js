bc8c676b00e1fa51e93f28741354db6a340939e0
iKiray <exkiray@gmail.com>
2026-06-16 19:40:28 -0400

    added grid making loop

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,11 @@
 //Step 2 - selecting the container div
 const container = document.getElementById("container");
+
+//Step 2 - creating the 16x16 div
+const size = 16;
+const total = size * size;
+
+for (let i = 0; i < total; i++) {
+    const div = document.createElement("div");
+    container.appendChild(div);
+}
