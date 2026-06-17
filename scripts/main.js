057ff14c64824c2234916787caa9ecb9651191e7
iKiray <exkiray@gmail.com>
2026-06-16 21:32:33 -0400

    refactor: removed css reliance and made it mainly js scripts 1. change values and css styles for inital 16x16 grid on load 2. removed .drawn class add on mouse event for drawing the divs and moved it to js script

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,21 @@
 const container = document.getElementById("container");
 
 //Step 2 - creating the 16x16 div
-const size = 16;
-const total = size * size;
-
-for (let i = 0; i < total; i++) {
+//Step 4 - refactored for inital grid, minus CSS reliance
+const initSize = 16;
+const initTotal = initSize * initSize;
+for (let i = 0; i < initTotal; i++) {
     const div = document.createElement("div");
+    
+    //Step 4 - making inital grid not reliant on css and js mainly
+    div.style.border = '1px solid black';
+    div.style.flex = `0 0 calc(100% / ${initSize})`;   // variable used here
+    div.style.aspectRatio = '1';
+    div.style.boxSizing = 'border-box';
     //Step 3 - applying a class to the div which happens during mouse enter event
+    //Step 4 - refactored to not have a class added but inline css colour change
     div.addEventListener("mouseenter", () => {
-        div.classList.add("drawn");
+        div.style.backgroundColor = "blue";
     })
     container.appendChild(div);
 }
@@ -20,5 +27,4 @@ const createBtn = document.getElementById("createGridBtn");
 createBtn.addEventListener("click", () => {
     let size = prompt("Type the number [X] for the grid [X by X] (maximum 100x100)");
 
-    
 })
